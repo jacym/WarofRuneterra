55eee259c1a9eabf34cb58d01a69e1815d59400f
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2019-11-19 19:07:33 -0800

    feat: read card set from json file

--- server/main.go
+++ server/main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/caarlos0/env"
+	"github.com/gorilla/mux"
+	"github.com/jacym/WarofRuneterra/server/dragon"
+)
 
 var (
 	cfg Config
+	set []dragon.Card
 )
 
-func cards(w http.ResponseWriter, r *http.Request) {
+func readCardSet() {
+	set = dragon.CardSet()
+}
+
+func indexCardSet(w http.ResponseWriter, r *http.Request) {
+	enc := json.NewEncoder(w)
+
+	if err := enc.Encode(&set); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+}
+
+func submitPlayerCards(w http.ResponseWriter, r *http.Request) {
 	var cardList []string
 
 	defer r.Body.Close()
@@ -24,6 +48,20 @@ func cards(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func cards(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		indexCardSet(w, r)
+		break
+	case "POST":
+		submitPlayerCards(w, r)
+		break
+	default:
+		http.Error(w, http.StatusText(405), 405)
+		break
+	}
+}
+
 func main() {
 	fmt.Println("WLoR v0.0.1")
 
@@ -32,13 +70,16 @@ func main() {
 		panic(err)
 	}
 
+	// card set
+	readCardSet()
+
 	// router
 	r := mux.NewRouter()
-	r.HandleFunc("/cards", cards)
+	r.HandleFunc("/cards", cards).Methods("GET", "POST")
 
 	srv := &http.Server{
 		Handler: r,
-		Addr: "127.0.0.1:" + cfg.Port
+		Addr:    "127.0.0.1:" + cfg.Port,
 
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
