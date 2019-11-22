cbfe6c64515e4c1295970052f75d6ba52bb2a3e7
Jacy <jacygmark@gmail.com>
2019-11-21 19:09:57 -0700

    attacker context identifier

---
+++ client/client.py
@@ -0,0 +1,91 @@
+import requests
+import time
+import yaml
+
+#TODO
+#add turn detection to determine when turns switch
+    #using turn detection commit units to a played set
+#send final set to server
+#make in play detection variable to different resolutions for future proof
+
+
+'''
+check if a card is on the battlefield
+:param cardPos: TopLeftY of card, INT
+:param screenHeight: height of play screen INT
+:param screenWidth: width of play screen INT
+:return: True if card is at a played Y position, False if not at a played Y position
+'''
+def inPlay(cardPos, screenHeight, screenWidth):
+    inPlayLocal = round(screenHeight * 0.41666666666)
+    inPlayOpp = round(screenHeight * 0.73148148148)
+
+    if cardPos == inPlayLocal or cardPos == inPlayOpp:
+        return True
+    else:
+        return False
+'''
+check which side is attacking by which side plays cards onto the battlefield first
+:param allCards: list of rectangle dictionaries with cardID, CardCode,top left X and Y, Card width and Height, and which player has it
+:param height: height of play screen INT
+:param width: width of play screen INT
+:return: 1 if attacker should be local player, -1 if attacker is the opponent, 0 if unsure (both sides have no play, both sides populated, etc)
+'''
+def attackerCheck(allCards, height, width):
+    local = []
+    opponent = []
+    for rectangle in allCards:
+        #local player card in play
+        if rectangle['LocalPlayer'] == True and inPlay(rectangle['TopLeftY'], height, width):
+            local.append(rectangle['CardID'])
+        #opponent card in play
+        elif rectangle['LocalPlayer'] == False and inPlay(rectangle['TopLeftY'], height, width):
+            opponent.append(rectangle['CardID'])
+    if len(local) > 0 and len(opponent) == 0:
+        return 1
+    elif len(opponent) > 0 and len(local) == 0:
+        return -1
+    else:
+        return 0
+
+def main():
+    print("began polling")
+    gamesPlayed = 0
+    gameState = 0
+    while True:
+        #only poll once a second
+        time.sleep(1)
+        #get card position and game result data
+        try:
+            req = requests.get("http://localhost:21337/positional-rectangles")
+            reqGame = requests.get('http://localhost:21337/game-result')
+        except:
+            print('Lost Connection To Legends of Runeterra \n Closing War on Runeterra Client')
+            break
+        print("start process")
+        if req.status_code == 200:
+            received = yaml.safe_load(req.text)
+            print(received)
+            if received['GameState'] == 'InProgress':
+                #find and set attacker states
+                attack = attackerCheck(received['Rectangles'], received['Screen']['ScreenHeight'], received['Screen']['ScreenWidth'])
+                if attack == 1:
+                    gameState = attack
+                elif attack == -1:
+                    gameState = attack
+                print(gameState)
+
+                
+                for rectangle in received['Rectangles']:
+                    if rectangle['LocalPlayer'] == True:
+                        print(rectangle['CardCode'], '\t', rectangle['TopLeftX'], '\t', rectangle['TopLeftY'])
+            elif received['GameState'] == 'Menus':
+                gameState = yaml.safe_load(reqGame.text)
+                print(gameState)
+                if gameState["GameID"] > gamesPlayed:
+                    gamesPlayed = gameState['GameID']
+                    print(gameState['LocalPlayerWon'])
+                    #send completed game card set to server here
+        
+
+main()
